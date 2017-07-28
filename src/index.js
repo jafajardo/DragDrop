ab7ffe7b4931d9534441b39984efe9596cfa56bd
Joseph Fajardo <jafajardo@gmail.com>
2017-07-28 12:32:20 +1000

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import RootReducer from './reducers';
+import Routes from './routes';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(RootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+  , document.querySelector('.container')
+);
---
+++ src/routes.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Router, browserHistory } from 'react-router';
+
+import App from './components/app';
+import Home from './components/home';
+
+const loadModule = (cb) => {
+  return module => cb(null, module.default);
+}
+
+const componentRoutes = {
+  component: App,
+  path: '/',
+  indexRoute: { component: Home },
+  childRoutes: [
+    {
+      path: 'page1',
+      getComponent(location, cb) {
+        System.import('./components/page1')
+          .then(loadModule(cb));
+      }
+    },
+    {
+      path: 'page2',
+      getComponent(location, cb) {
+        System.import('./components/page2')
+          .then(loadModule(cb));
+      }
+    }
+  ]
+}
+
+const Routes = () => {
+  return (
+    <Router history={browserHistory} routes={componentRoutes} />
+  );
+}
+
+export default Routes;
